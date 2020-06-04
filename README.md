ef3e9321f7edea8c7ae8b30cc93e8a85ad4dd685
Sahin Tas <sahin.tas@kit.edu>
2020-06-04 13:29:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# MP SIM
+
+Main pkg for motion-planning-sim
+
+### [Doxygen documentation](http://mrt.pages.mrt.uni-karlsruhe.de/private/tas/simulation/mp_sim/doxygen/index.html)
+### [Coverage report](http://mrt.pages.mrt.uni-karlsruhe.de/private/tas/simulation/mp_sim/coverage/index.html)
+
+## Usage
+
+TODO: Write usage instructions
---
+++ test/mp_sim.cpp
@@ -0,0 +1,15 @@
+// google test docs
+// wiki page: https://code.google.com/p/googletest/w/list
+// primer: https://code.google.com/p/googletest/wiki/V1_7_Primer
+// FAQ: https://code.google.com/p/googletest/wiki/FAQ
+// advanced guide: https://code.google.com/p/googletest/wiki/V1_7_AdvancedGuide
+// samples: https://code.google.com/p/googletest/wiki/V1_7_Samples
+//
+#include "gtest/gtest.h"
+
+// A google test function (uncomment the next function, add code and
+// change the names TestGroupName and TestName)
+// TEST(${pkgname}, TestName) {
+//     EXPECT_TRUE(true);
+//     TODO: Add your test code here
+//}
