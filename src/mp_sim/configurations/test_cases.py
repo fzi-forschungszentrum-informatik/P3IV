41b92f71f2f86dfc5e03814c2cdb459bad8cbb1a
Sahin Tas <sahin.tas@kit.edu>
2020-10-29 16:38:04 +0100

    add new test case definition

--- src/mp_sim/configurations/test_cases.py
+++ src/mp_sim/configurations/test_cases.py
@@ -9,5 +9,14 @@ test_cases = {
         "toLanelet": dict([(1, "30008"),  # closed-loop planning is performed only for vehicles that are defined here
                            (2, "30008"),
                            (3, "30008")])
+    },
+    "DEU_Merging_OL_01": {
+        "source": "interaction_sim",
+        "map": 'DR_DEU_Merging_MT_extended',
+        "timestamp_begin": 4400,
+        "timestamp_end": 4400,
+        "vehicle_of_interest": 1,
+        "open_loop": True,
+        "toLanelet": dict([(1, "30008")])
     }
 }
