5386455d4ca17204e017e72982bbc1ed13bde55f
Sahin Tas <sahin.tas@kit.edu>
2021-02-01 11:00:25 +0100

    apply pep8 formatiing

--- src/mp_sim/drive.py
+++ src/mp_sim/drive.py
@@ -11,32 +11,39 @@ logger.setLevel(logging.INFO)
 def drive(vehicle, ground_truth):
 
     Print2Console.p('s', ['-' * 72], style='cyan', bold=True, first_col_w=40)
-    Print2Console.p('ss', ['Computing vehicle: ', vehicle.v_id], style='cyan', bold=True, first_col_w=40)
+    Print2Console.p('ss', ['Computing vehicle: ', vehicle.v_id],
+                    style='cyan', bold=True, first_col_w=40)
     Print2Console.p('s', ['-' * 72], style='cyan', bold=True, first_col_w=40)
 
+    # get the current timestampdata
     timestampdata = vehicle.timestamps.latest()
 
     logger.debug('Nodes for computation: ')
     logger.debug(timestampdata.motion.cartesian.position.mean[-4:])
 
-    # Localization -----------------------------------------------------------------------------------------------------
-    timestampdata.localization = vehicle.modules.localization(timestampdata.motion.cartesian)
+    # Localization ------------------------------------------------------------
+    timestampdata.localization = vehicle.modules.localization(
+        timestampdata.motion.cartesian)
 
-    # Perception -------------------------------------------------------------------------------------------------------
-    timestampdata.environment = vehicle.modules.perception(ground_truth, timestampdata.motion.pose[-1])
+    # Perception --------------------------------------------------------------
+    timestampdata.environment = vehicle.modules.perception(
+        ground_truth, timestampdata.motion.pose[-1])
 
-    # Understanding ----------------------------------------------------------------------------------------------------
-    timestampdata.scene = vehicle.modules.understanding(timestampdata.environment, timestampdata.motion)
+    # Understanding -----------------------------------------------------------
+    timestampdata.scene = vehicle.modules.understanding(
+        timestampdata.environment, timestampdata.motion)
 
-    # Prediction--- ----------------------------------------------------------------------------------------------------
+    # Prediction--- -----------------------------------------------------------
     timestampdata.situation = vehicle.modules.prediction(timestampdata.scene)
 
-    # Decision Making --------------------------------------------------------------------------------------------------
-    timestampdata.decision_base = vehicle.modules.decision(timestampdata.motion, timestampdata.scene, timestampdata.situation)
+    # Decision Making ---------------------------------------------------------
+    timestampdata.decision_base = vehicle.modules.decision(
+        timestampdata.motion, timestampdata.scene, timestampdata.situation)
 
-    # Motion Planning --------------------------------------------------------------------------------------------------
-    timestampdata.motion_plans = vehicle.modules.planner(timestampdata.decision_base)
+    # Motion Planning ---------------------------------------------------------
+    timestampdata.motion_plans = vehicle.modules.planner(
+        timestampdata.decision_base)
 
-    # Pick the optimal action ------------------------------------------------------------------------------------------
-    timestampdata.plan_optimal = vehicle.modules.action(timestampdata.motion, timestampdata.motion_plans)
-
+    # Pick the optimal action -------------------------------------------------
+    timestampdata.plan_optimal = vehicle.modules.action(
+        timestampdata.motion, timestampdata.motion_plans)
