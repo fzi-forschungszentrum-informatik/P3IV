08c49ef410a6d33623d4b0fd4eb67b7341bb751c
Sahin Tas <sahin.tas@kit.edu>
2021-04-22 20:17:13 +0200

    Allow some modules be not set

--- src/mp_sim/modules.py
+++ src/mp_sim/modules.py
@@ -1,52 +1,78 @@
-from perception.main import Percept
-from localization.main import Localization
-from understanding.main import Understand
-from prediction.main import Predict
-from decision_making.main import Decide
-from planner.main import Plan
-from action.main import Act
-
-
 class VehicleModules(object):
     def __init__(self, configurations, laneletmap, vehicle):
 
-        self.localization = Localization(configurations["temporal"]["dt"],
-                                         measurement_noise=configurations["localization"]["measurement_noise"],
-                                         process_noise=configurations["localization"]["process_noise"])
+        # set localization
+        try:
+            from localization.main import Localization
+            self.localization = Localization(configurations["temporal"]["dt"],
+                                             measurement_noise=configurations["localization"]["measurement_noise"],
+                                             process_noise=configurations["localization"]["process_noise"])
+        except ImportError as e:
+            print(str(e))
 
-        self.perception = Percept(laneletmap,
-                                  vehicle._v_id,
-                                  vehicle.perception.sensor_fov,
-                                  vehicle.perception.sensor_range,
-                                  vehicle.perception.sensor_noise,
-                                  override_visibility=configurations['perception']['override_visibility'])
+        # set perception
+        try:
+            from perception.main import Percept
+            self.perception = Percept(laneletmap,
+                                      vehicle._v_id,
+                                      vehicle.perception.sensor_fov,
+                                      vehicle.perception.sensor_range,
+                                      vehicle.perception.sensor_noise,
+                                      override_visibility=configurations['perception']['override_visibility'])
+        except ImportError as e:
+            print(str(e))
 
-        self.understanding = Understand(configurations["temporal"]["dt"],
-                                        configurations["temporal"]
-                                        ["N"],
-                                        laneletmap,
-                                        vehicle._v_id,
-                                        toLanelet=vehicle.objective.toLanelet)
+        # set understanding
+        try:
+            from understanding.main import Understand
+            self.understanding = Understand(configurations["temporal"]["dt"],
+                                            configurations["temporal"]
+                                            ["N"],
+                                            laneletmap,
+                                            vehicle._v_id,
+                                            toLanelet=vehicle.objective.toLanelet)
+        except ImportError as e:
+            print(str(e))
 
-        self.prediction = Predict(configurations["temporal"]["dt"],
-                                  configurations["temporal"]["N"],
-                                  configurations["map"],
-                                  configurations["prediction"])
+        # set prediction
+        try:
+            from prediction.main import Predict
+            self.prediction = Predict(configurations["temporal"]["dt"],
+                                      configurations["temporal"]["N"],
+                                      configurations["map"],
+                                      configurations["prediction"])
+        except ImportError as e:
+            print(str(e))
 
-        self.decision = Decide(vehicle.characteristics.max_acceleration,
-                               vehicle.characteristics.max_deceleration,
-                               vehicle.objective.set_speed,
-                               configurations["temporal"]["dt"],
-                               configurations["temporal"]["N"],
-                               configurations["temporal"]["N_pin_future"],
-                               configurations["decision_making"]["astar_initialization"])
+        # set decision
+        try:
+            from decision_making.main import Decide
+            self.decision = Decide(vehicle.characteristics.max_acceleration,
+                                   vehicle.characteristics.max_deceleration,
+                                   vehicle.objective.set_speed,
+                                   configurations["temporal"]["dt"],
+                                   configurations["temporal"]["N"],
+                                   configurations["temporal"]["N_pin_future"],
+                                   configurations["decision_making"]["astar_initialization"])
+        except ImportError as e:
+            print(str(e))
 
-        self.planner = Plan(configurations,
-                            vehicle.characteristics.max_acceleration,
-                            vehicle.characteristics.max_deceleration,
-                            get_planner_type(configurations, vehicle))
+        # set planner
+        try:
+            from planner.main import Plan
+            self.planner = Plan(configurations,
+                                vehicle.characteristics.max_acceleration,
+                                vehicle.characteristics.max_deceleration,
+                                get_planner_type(configurations, vehicle))
+        except ImportError as e:
+            print(str(e))
 
-        self.action = Act()
+        # set action
+        try:
+            from action.main import Act
+            self.action = Act()
+        except ImportError as e:
+            print(str(e))
 
 
 def get_planner_type(configurations, vehicle):
