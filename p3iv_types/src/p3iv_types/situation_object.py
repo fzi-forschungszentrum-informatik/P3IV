8c90587f3760e04f0886d9974a14680401e0b25d
Sahin Tas <sahin.tas@kit.edu>
2021-05-09 11:52:26 +0200

    Add slots to SituationObject

--- p3iv_types/src/p3iv_types/situation_object.py
+++ p3iv_types/src/p3iv_types/situation_object.py
@@ -1,15 +1,20 @@
 from __future__ import division
-from prediction.maneuver_identifier.types.maneuver_hypotheses import ManeuverHypotheses
+import warnings
 from p3iv_types.scene_object import SceneObject
 
 
 class SituationObject(object):
-    def __init__(self, *args, **kwargs):
-        self.v_id = None
-        self.maneuvers = ManeuverHypotheses()  # current maneuver hypothesis of the current timestep
-        self.crossing_voi_route = None
+    """
+    v_id: int
+        Vehicle ID
+    maneuvers: ManeuverHypotheses
+        Current maneuver hypothesis of the current timestep
+    """
 
+    __slots__ = ["v_id", "maneuvers", "crossing_voi_route"]
+
+    def __init__(self, *args, **kwargs):
         if type(args[0]) is SceneObject:
             self.v_id = args[0].v_id
+            warnings.warn("todo - crossing voi route of situation object --> do that for path options")
             self.crossing_voi_route = args[0].crossing_voi_route
-            # raw_input("todo - crossing voi route of situation object --> do that for path options")
