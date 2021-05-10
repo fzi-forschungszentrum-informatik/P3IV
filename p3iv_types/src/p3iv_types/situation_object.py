a915c5048f840bb5c285ed5b8d6de5c885dd01ba
Sahin Tas <sahin.tas@kit.edu>
2021-05-10 20:31:05 +0200

    Add setters & getters to SituationObject

--- p3iv_types/src/p3iv_types/situation_object.py
+++ p3iv_types/src/p3iv_types/situation_object.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import warnings
+import itertools
 from p3iv_types.scene_object import SceneObject
 from p3iv_types.tracked_object import TrackedObject
 
@@ -12,7 +13,7 @@ class SituationObject(TrackedObject):
         Current maneuver hypothesis of the current timestep
     """
 
-    __slots__ = ["maneuvers", "crossing_voi_route"]
+    __slots__ = ["_maneuvers", "_crossing_voi_route"]
 
     def __init__(self, *args, **kwargs):
         if type(args[0]) is SceneObject:
@@ -21,3 +22,32 @@ class SituationObject(TrackedObject):
             self.crossing_voi_route = args[0].crossing_voi_route
 
     # todo add init with... See prediction test cases! Make this more generic
+
+    def __setattr__(self, name, value):
+        # modify setattr for multiple inheritence
+
+        # get all slots
+        all_slots = list(itertools.chain.from_iterable(getattr(t, "__slots__", ()) for t in type(self).__mro__))
+
+        # cast slots iterable to list
+        if name in all_slots:
+            object.__setattr__(self, name, value)
+        else:
+            # call property if name is not in slots
+            super(SituationObject, self).__setattr__(name, value)
+
+    @property
+    def maneuvers(self):
+        return self._maneuvers
+
+    @maneuvers.setter
+    def maneuvers(self, maneuvers):
+        self._maneuvers = maneuvers
+
+    @property
+    def crossing_voi_route(self):
+        return self._crossing_voi_route
+
+    @crossing_voi_route.setter
+    def crossing_voi_route(self, crossing_voi_route):
+        self._crossing_voi_route = crossing_voi_route
