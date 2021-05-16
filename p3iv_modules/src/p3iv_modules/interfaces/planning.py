44c690ffaf57f8c22fcf1bc1f1f8584531a30064
Sahin Tas <sahin.tas@kit.edu>
2021-05-16 22:04:28 +0200

    Add documentation

--- p3iv_modules/src/p3iv_modules/interfaces/planning.py
+++ p3iv_modules/src/p3iv_modules/interfaces/planning.py
@@ -11,6 +11,28 @@ class PlannerInterface(object):
 
     @abc.abstractmethod
     def __call__(self, timestamp, motion_state, scene_model, situation_model, decision_base, *args, **kwargs):
+        """
+        Perform planning given current scene model, situation model and decision base.
+
+        Parameters
+        ----------
+        timestamp: int
+            Current timestamp value.
+        motion_state: MotionState
+            Current motion state of the vehicle.
+        scene_model: SceneModel
+            Scene model built up for the current timestamp.
+        situation_model: SituationModel
+            Situation model that includes prediction of vehicles in the scene_model.
+        decision_base: DecisionBase
+            Decision base that includes driving intentions of the ego vehicle.
+
+        Returns
+        -------
+        motion_plans: MotionPlans
+            A container that contains motions of different homotopy classes.
+
+        """
         self.type_check(timestamp, motion_state, scene_model, situation_model, decision_base)
         pass
 
