7005ac8233a35937992b579557c7d1860721910d
Sahin Tas <sahin.tas@kit.edu>
2021-05-16 23:04:41 +0200

    Housekeeping

---
+++ p3iv_types/test/test_motionplan.py
@@ -0,0 +1,18 @@
+import unittest
+from p3iv_types.motion_plans import MotionPlan, MotionPlans
+
+
+class MotionPlansTest(unittest.TestCase):
+    def test_empty(self):
+        MotionPlans()
+
+    def test_mp(self):
+        mp1 = MotionPlan()
+        mp2 = MotionPlan()
+        mps = MotionPlans()
+        mps.append(mp1)
+        mps.append(mp2)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- p3iv_utils/test/test_vehicle_corners.py
+++ p3iv_utils/test/test_vehicle_corners.py
@@ -1,19 +1,14 @@
 import unittest
 import numpy as np
 import matplotlib.pyplot as plt
-from p3iv_types.vehicle import Vehicle, VehicleRectangle
+from p3iv_utils.vehicle_rectangle import VehicleRectangle
 
 
 class TestVehicleCorners(unittest.TestCase):
-    def setUp(self):
-        self.vehicle = Vehicle(0)
-        self.vehicle.appearance.length = 5.2
-        self.vehicle.appearance.width = 1.8
-
     def test_corners(self):
         xy = np.array([3, 3])
         for theta in np.linspace(-420, 420, 100):
-            VehicleRectangle.get_corners(self.vehicle.appearance.length, self.vehicle.appearance.width, xy, theta)
+            VehicleRectangle.get_corners(3.2, 1.6, xy, theta)
 
     def test_corners_w_plot(self):
         xy = np.array([3, 3])
@@ -24,7 +19,7 @@ class TestVehicleCorners(unittest.TestCase):
         plt.gca().set_aspect("equal", adjustable="box")
 
         for theta in np.linspace(-420, 420, 50):
-            c = VehicleRectangle.get_corners(self.vehicle.appearance.length, self.vehicle.appearance.width, xy, theta)
+            c = VehicleRectangle.get_corners(3.2, 1.6, xy, theta)
             for c_ in c:
                 plt.scatter(c_[0], c_[1], color="red", marker="o")
         plt.show()
