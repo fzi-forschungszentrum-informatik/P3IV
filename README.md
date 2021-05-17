9a9261a83b36f2ad38c322bdbe654a095307635c
Sahin Tas <sahin.tas@kit.edu>
2021-05-17 15:08:45 +0200

    Revise README and install.sh

--- README.md
+++ README.md
@@ -17,3 +17,24 @@ python main.py --predict=DEU_Roundabout_OL_01
 python main.py --run=DEU_Roundabout_OL_01
 
 ```
+## Install
+
+```
+#!/bin/sh
+pip install requirements.txt
+source /opt/ros/$ROS_DISTRO/setup.bash
+mkdir catkin_ws && cd catkin_ws && mkdir src
+catkin init
+catkin config --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo # build in release mode (or whatever you prefer)
+```
+
+```
+git clone https://gitlab.mrt.uni-karlsruhe.de/pub/mrt_cmake_modules.git
+git clone https://gitlab.mrt.uni-karlsruhe.de/planning-simulation/understanding.git
+git clone https://gitlab.mrt.uni-karlsruhe.de/planning-simulation/polygon_geometry.git
+git clone https://gitlab.mrt.uni-karlsruhe.de/planning-simulation/prediction.git
+git clone https://gitlab.mrt.uni-karlsruhe.de/planning-simulation/util_probability.git
+git clone https://gitlab.mrt.uni-karlsruhe.de/planning-simulation/util_motion.git
+git clone https://gitlab.mrt.uni-karlsruhe.de/planning-simulation/decision_making.git
+git clone https://gitlab.mrt.uni-karlsruhe.de/planning-simulation/planner.git
+```
--- install.sh
+++ install.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 pip install requirements.txt
-source /opt/ros/$ROS_DISTRO/setup.bash
-mkdir catkin_ws && cd catkin_ws && mkdir src
-catkin init
-catkin config --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo # build in release mode (or whatever you prefer)
-cd src
-git clone https://github.com/KIT-MRT/mrt_cmake_modules.git
-# git clone https://github.com/fzi-forschungszentrum-informatik/lanelet2.git
+git clone https://gitlab.mrt.uni-karlsruhe.de/pub/mrt_cmake_modules.git
 git clone https://gitlab.mrt.uni-karlsruhe.de/planning-simulation/understanding.git
 git clone https://gitlab.mrt.uni-karlsruhe.de/planning-simulation/polygon_geometry.git
 git clone https://gitlab.mrt.uni-karlsruhe.de/planning-simulation/prediction.git
@@ -14,5 +8,3 @@ git clone https://gitlab.mrt.uni-karlsruhe.de/planning-simulation/util_probabili
 git clone https://gitlab.mrt.uni-karlsruhe.de/planning-simulation/util_motion.git
 git clone https://gitlab.mrt.uni-karlsruhe.de/planning-simulation/decision_making.git
 git clone https://gitlab.mrt.uni-karlsruhe.de/planning-simulation/planner.git
-catkin build
-
