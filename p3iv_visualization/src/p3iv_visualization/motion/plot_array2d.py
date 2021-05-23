475b3baf744a949932598401e8b9f115e2eb32fa
Sahin Tas <sahin.tas@kit.edu>
2021-05-23 12:24:47 +0200

    Refactor: define a separate method for plotting magnitude

--- p3iv_visualization/src/p3iv_visualization/motion/plot_array2d.py
+++ p3iv_visualization/src/p3iv_visualization/motion/plot_array2d.py
@@ -29,19 +29,7 @@ class PlotArray2D(object):
 
         self._update_motion_array(self.ax_x_pinn, self.ax_x_free, motion_array2d[:, 0], index4pin2free=index4pin2free)
         self._update_motion_array(self.ax_y_pinn, self.ax_y_free, motion_array2d[:, 1], index4pin2free=index4pin2free)
-
-        if magnitude_flag:
-            magnitude = np.linalg.norm(motion_array2d, axis=1)
-            if index4pin2free > 0:
-                self.ax_m_pinn.set_data(self.t[:index4pin2free], magnitude[:index4pin2free])
-                self.ax_m_free.set_data(self.t[index4pin2free - 1 :], magnitude[index4pin2free - 1 :])
-            else:
-                self.ax_m_pinn.set_data(self.t, magnitude)
-
-        else:
-            # the last magnitude line remains on the plot, if this block is deleted!
-            self.ax_m_free.set_data([], [])
-            self.ax_m_pinn.set_data([], [])
+        self._update_magnitude_array(motion_array2d, index4pin2free, magnitude_flag)
 
     def set_x_axis(self, increment=2, set_ticks=True):
         """
@@ -70,7 +58,7 @@ class PlotArray2D(object):
         self.ax.set_ylim(lim_min, lim_max)
         self.ax.set_yticks(np.arange(lim_min, lim_max, increment))
 
-    def _update_motion_array(self, ax_pinn, ax_free, data, index4pin2free=0):
+    def _update_motion_array(self, ax_pinn, ax_free, data, index4pin2free):
 
         if index4pin2free > 0:
             data_pinn = data[:index4pin2free]
@@ -82,6 +70,20 @@ class PlotArray2D(object):
         else:
             # data_pinn is equal to data
             ax_pinn.set_data(self.t, data)
+
+    def _update_magnitude_array(self, data, index4pin2free, magnitude_flag):
+        if magnitude_flag:
+            magnitude = np.linalg.norm(data, axis=1)
+            if index4pin2free > 0:
+                self.ax_m_pinn.set_data(self.t[:index4pin2free], magnitude[:index4pin2free])
+                self.ax_m_free.set_data(self.t[index4pin2free - 1 :], magnitude[index4pin2free - 1 :])
+            else:
+                self.ax_m_pinn.set_data(self.t, magnitude)
+
+        else:
+            # the last magnitude line remains on the plot, if this block is deleted!
+            self.ax_m_free.set_data([], [])
+            self.ax_m_pinn.set_data([], [])
 
 
 if __name__ == "__main__":
