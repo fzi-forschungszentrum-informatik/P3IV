645de37df2985e2e53f5c7b61fbd3c330e424d98
Sahin Tas <sahin.tas@kit.edu>
2021-06-12 12:11:03 +0200

    Add understanding interface

--- p3iv_modules/src/p3iv_modules/interfaces/__init__.py
+++ p3iv_modules/src/p3iv_modules/interfaces/__init__.py
@@ -2,3 +2,4 @@ from planning import PlannerInterface
 from perception import PerceptInterface
 from prediction import PredictInterface
 from decision_making import DecisionMakingInterface
+from understanding import SceneUnderstandingInterface
---
+++ p3iv_modules/src/p3iv_modules/interfaces/understanding.py
@@ -0,0 +1,50 @@
+import abc
+import numpy as np
+import p3iv_types
+
+
+class SceneUnderstandingInterface(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def __init__(self, dt, N, laneletmap, vehicle_id, toLanelet=None, *args, **kwargs):
+        """
+        Parameters
+        ----------
+        dt: double
+            Sampling-time in [ms]
+        N: int
+            Number of timesteps in the horizon
+        laneletmap: Lanelet2-Map
+            Lanelet2 map of the current
+        vehicle_id: int
+            ID of ego vehicle
+        toLanelet: int
+            Goal lanelet ID to reach
+        """
+        pass
+
+    @abc.abstractmethod
+    def __call__(self, tracked_vehicles, *args, **kwargs):
+        """
+        Run scene understanding given tracked vehicles list.
+
+        Parameters
+        ----------
+        tracked_vehicles: list
+            A list of TrackedObject type
+        Returns
+        -------
+        scene_model: SceneModel
+            Scene model with curent lanelets of ego vehicle and percepted objects.
+        """
+        self.type_check(tracked_vehicles)
+        pass
+
+    @staticmethod
+    def type_check(tracked_vehicles):
+        """
+        Utility type check function.
+        """
+        assert isinstance(tracked_vehicles, (list, np.ndarray))
+        assert isinstance(tracked_vehicles[0], p3iv_types.TrackedObject)
