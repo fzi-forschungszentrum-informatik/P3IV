Bugfix angle comparison in relative distance sign

p3iv_modules/src/p3iv_modules/understanding/basic.py
@@ -163,7 +163,7 @@ class Understand(SceneUnderstandingInterface):
         # get sign
         pos_diff = guest_state.position.mean - host_state.position.mean
         angle = (np.rad2deg(np.arctan2(pos_diff[1], pos_diff[0])) + 360.0) % 360.0
-        if 360.0 > angle > 170.0 or 10.0 > angle > 0.0:
+        if 270.0 > angle > 90.0:
             # vehicle is behind ego vehicle
             v2v_distance = v2v_distance * -1.0
 