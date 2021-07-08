378ef7bd073450fc1c460f85d58135a11a498ec3
Sahin Tas <sahin.tas@kit.edu>
2021-07-08 22:37:00 +0200

    Add vehicle model to calculate steering angle

---
+++ p3iv_utils/src/p3iv_utils/vehicle_models.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def get_control_inputs(yaw_angle, speed, wheelbase, dt):
+    """
+    Kinematic vehicle model
+    http://planning.cs.uiuc.edu/node658.html
+    """
+
+    acc = np.diff(speed) * (1.0 / dt)
+    acc = np.append(acc, acc[-1])
+
+    yaw_r = np.deg2rad(np.diff(yaw_angle) / dt)[1:]
+    steering_angle = np.rad2deg(np.arctan((yaw_r * wheelbase) / speed[2:]))
+    steering_angle = np.append(steering_angle, steering_angle[-1])
+    # todo: TAKE CARE OF THIS APPROXIMATION HERE: INDICES!
+    steering_angle = np.append(steering_angle, steering_angle[-1])
+    controls = np.empty([len(acc), 2])
+    controls[:, 0] = steering_angle
+    controls[:, 1] = acc
+    return controls
